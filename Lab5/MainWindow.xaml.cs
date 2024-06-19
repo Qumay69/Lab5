9f7ad07b905bcbfa890e419d625e9b90f2b74fab
Qumay <Qumay@DESKTOP-1CSQH0Q>
2024-06-20 00:11:26 +0200

    Добавьте файлы проекта.

---
+++ Lab5/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab5
+{
+    public partial class MainWindow : Window
+    {
+        private List<NOTE<object>> notes = new List<NOTE<object>>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string lastName = LastNameTextBox.Text;
+                string firstName = FirstNameTextBox.Text;
+                string phoneNumber = PhoneNumberTextBox.Text;
+                int[] birthDate = BirthDateTextBox.Text.Split('.').Select(int.Parse).ToArray();
+
+                var note = new NOTE<object>(lastName, firstName, phoneNumber, birthDate);
+                notes.Add(note);
+                notes.Sort();
+                DisplayNotes();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка при добавлении записи: " + ex.Message);
+            }
+        }
+
+        private void SearchButton_Click(object sender, RoutedEventArgs e)
+        {
+            string searchLastName = SearchLastNameTextBox.Text;
+            var foundNote = notes.FirstOrDefault(n => n.LastName.Equals(searchLastName, StringComparison.OrdinalIgnoreCase));
+
+            if (foundNote != null)
+            {
+                MessageBox.Show(foundNote.ToString());
+            }
+            else
+            {
+                MessageBox.Show("Человек с такой фамилией не найден.");
+            }
+        }
+
+        private void DisplayNotes()
+        {
+            NotesListBox.Items.Clear();
+            foreach (var note in notes)
+            {
+                NotesListBox.Items.Add(note);
+            }
+        }
+    }
+}
---
+++ Lab5/NOTE.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5
+{
+    public class NOTE<T> : ICloneable, IComparable<NOTE<T>>
+    {
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public T PhoneNumber { get; set; }
+        public int[] BirthDate { get; set; } // [day, month, year]
+
+        public NOTE() { }
+
+        public NOTE(string lastName, string firstName, T phoneNumber, int[] birthDate)
+        {
+            LastName = lastName;
+            FirstName = firstName;
+            PhoneNumber = phoneNumber;
+            BirthDate = birthDate;
+        }
+
+        public object Clone()
+        {
+            return new NOTE<T>(LastName, FirstName, PhoneNumber, (int[])BirthDate.Clone());
+        }
+
+        public int CompareTo(NOTE<T> other)
+        {
+            string thisPhoneStart = PhoneNumber.ToString().Substring(0, 3);
+            string otherPhoneStart = other.PhoneNumber.ToString().Substring(0, 3);
+            return string.Compare(thisPhoneStart, otherPhoneStart, StringComparison.Ordinal);
+        }
+
+        public override string ToString()
+        {
+            return $"{LastName} {FirstName}, Phone: {PhoneNumber}, Birth Date: {BirthDate[0]}.{BirthDate[1]}.{BirthDate[2]}";
+        }
+    }
+
+    public class NoteComparer : IComparer<NOTE<object>>
+    {
+        public int Compare(NOTE<object> x, NOTE<object> y)
+        {
+            string xFullName = $"{x.LastName} {x.FirstName}";
+            string yFullName = $"{y.LastName} {y.FirstName}";
+            return string.Compare(xFullName, yFullName, StringComparison.Ordinal);
+        }
+    }
+}
